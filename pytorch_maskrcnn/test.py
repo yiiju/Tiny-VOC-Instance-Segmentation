cbd4cb7be50b2b260655468fbeadb981273480ea
yiiju <yijuhung0129@gmail.com>
2020-12-08 17:57:16 +0800

    Use PEP8

--- pytorch_maskrcnn/test.py
+++ pytorch_maskrcnn/test.py
@@ -27,7 +27,7 @@ def get_transform():
         transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                std=[0.229, 0.224, 0.225])
+                                 std=[0.229, 0.224, 0.225])
         ])
     else:
         transform = transforms.Compose([
@@ -78,7 +78,7 @@ if __name__ == "__main__":
 
     model.load_state_dict(torch.load(args.checkpoint))
     model.to(device)
-    dataset = TinyDataset('../', get_transform(), mode="test")
+    dataset = TinyDataset('../data', get_transform(), mode="test")
     testLoader = torch.utils.data.DataLoader(
                     dataset, batch_size=args.batch_size,
                     shuffle=False, num_workers=4,
--- pytorch_maskrcnn/train.py
+++ pytorch_maskrcnn/train.py
@@ -28,7 +28,7 @@ def get_transform(train):
 
     if args.norm:
         transforms.append(T.Normalize(mean=[0.485, 0.456, 0.406],
-                                    std=[0.229, 0.224, 0.225]))
+                                      std=[0.229, 0.224, 0.225]))
     return T.Compose(transforms)
 
 
